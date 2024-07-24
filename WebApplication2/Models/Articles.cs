283694eb699b1c09f8686b566ad831f7825957d9
y.sokolova <y.sokolova@khai.edu>
2024-07-24 13:55:19 +0000

    Upload New File

---
+++ WebApplication2/Models/Articles.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class ArticlesContext:DbContext
+    {
+        public ArticlesContext(DbContextOptions<ArticlesContext> options):base(options)
+        {
+        }
+        public DbSet<Article> Articles { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("article");
+        }
+    }
+}
