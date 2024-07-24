6ae96d54c1e23707db1f037638254cb2ce8018ca
y.sokolova <y.sokolova@khai.edu>
2024-07-24 13:55:04 +0000

    Upload New File

---
+++ WebApplication2/Models/Article.cs
@@ -0,0 +1,28 @@
+﻿
+namespace WebApplication2.Models
+{
+      public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string  Author { get; set; }= string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty; 
+        public DateTime Date { get; set; }= DateTime.Now;
+        public string? FileName { get; set; } = string.Empty;
+        public Article()
+        {
+
+        }
+        public Article(int id, string title, string author, string category, string description, DateTime date, string fileName)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Category = category;
+            Description = description;
+            Date = date;
+            FileName = fileName;
+        }
+    }
+}
