4de1cf704107d64fa7259c2842e7fed8ef1b86bf
y.sokolova <y.sokolova@khai.edu>
2024-07-24 13:56:18 +0000

    Upload New File

---
+++ WebApplication2/DbInitializer.cs
@@ -0,0 +1,15 @@
+﻿using WebApplication2.Models;
+using Npgsql;
+public static class DbInitializer
+{
+    
+    public static void Initialize(ArticlesContext context)
+    {
+        
+        var articles = new List <Article>();
+       
+        context.Articles.AddRange(articles);
+        context.SaveChanges();
+
+    }
+}
