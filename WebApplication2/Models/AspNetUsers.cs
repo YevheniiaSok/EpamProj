6821081e7cd4f191f62d272ec135c4a99639397c
y.sokolova <y.sokolova@khai.edu>
2024-07-26 11:43:37 +0000

    Додано модель для авторизації

---
+++ WebApplication2/Models/AspNetUsers.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication2.Models
+{
+    public class AspNetUsers:IdentityUser
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
